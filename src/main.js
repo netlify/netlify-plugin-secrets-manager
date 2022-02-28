0d3276135eacb88aaae4505d98c65de2b8eb1592
Erez Rokah <erezrokah@users.noreply.github.com>
2022-02-28 17:03:32 +0100

    feat: context support (#4)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const process = require('process')
 
-const { SecretsManagerClient, ListSecretsCommand, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
+const {
+  SecretsManagerClient,
+  ListSecretsCommand,
+  GetSecretValueCommand,
+  DescribeSecretCommand,
+} = require('@aws-sdk/client-secrets-manager')
 const chalk = require('chalk')
 
 const listSecrets = async ({ client, nextToken, secrets = [] }) => {
@@ -14,23 +19,39 @@ const listSecrets = async ({ client, nextToken, secrets = [] }) => {
   return newSecrets
 }
 
-const normalizeSecretValue = async ({ client, secret }) => {
+const getSecretContext = async ({ client, secret }) => {
+  try {
+    const { Tags: tags = [] } = await client.send(new DescribeSecretCommand({ SecretId: secret.ARN }))
+    const context = tags.find(({ Key: key }) => key === 'NETLIFY_CONTEXT')
+    return context && context.Value
+  } catch (error) {
+    if (error.name === 'AccessDeniedException') {
+      return console.log(
+        chalk.dim(`Does not have permissions to retrieve context for secret ${chalk.yellow(secret.Name)}`),
+      )
+    }
+  }
+}
+
+const getSecretsValue = async ({ client, secret }) => {
   try {
     // SecretString is a JSON string representation of the secret, e.g. '{"SECRET_NAME":"SECRET_VALUE"}'
     const { SecretString: secretString } = await client.send(new GetSecretValueCommand({ SecretId: secret.ARN }))
+    const context = await getSecretContext({ client, secret })
     const parsedValue = JSON.parse(secretString)
-    return parsedValue
+    return Object.entries(parsedValue).map(([key, value]) => ({ key, value, context }))
   } catch (error) {
     if (error.name === 'AccessDeniedException') {
-      return console.log(chalk.dim(`Skipping restricted AWS secret ${chalk.yellow(secret.Name)}`))
+      console.log(chalk.dim(`Skipping restricted secret ${chalk.yellow(secret.Name)}`))
+      return []
     }
     throw error
   }
 }
 
-const normalizeSecrets = async ({ client, secrets }) => {
-  const values = await Promise.all(secrets.map((secret) => normalizeSecretValue({ client, secret })))
-  return Object.assign({}, ...values)
+const getSecretsValues = async ({ client, secrets }) => {
+  const secretsWithValues = await Promise.all(secrets.map((secret) => getSecretsValue({ client, secret })))
+  return secretsWithValues.flat()
 }
 
 const SECRET_PREFIX = process.env.NETLIFY_AWS_SECRET_PREFIX || 'NETLIFY_AWS_SECRET_'
@@ -43,6 +64,7 @@ module.exports = {
       NETLIFY_AWS_ACCESS_KEY_ID: accessKeyId,
       NETLIFY_AWS_SECRET_ACCESS_KEY: secretAccessKey,
       NETLIFY_AWS_DEFAULT_REGION: region = 'us-east-1',
+      CONTEXT,
     } = process.env
     if (!accessKeyId) {
       return utils.build.failBuild(`Missing environment variable NETLIFY_AWS_ACCESS_KEY_ID`)
@@ -57,14 +79,28 @@ module.exports = {
       credentials: { accessKeyId, secretAccessKey },
     })
     const secrets = await listSecrets({ client })
-    const normalizedSecrets = await normalizeSecrets({ client, secrets })
-
-    const entries = Object.entries(normalizedSecrets)
-    entries.forEach(([key, value]) => {
+    const secretsWithValues = await getSecretsValues({ client, secrets })
+    secretsWithValues.forEach(({ key, value, context }) => {
       const prefixedKey = getPrefixedKey(key)
-      console.log(`${chalk.bold('Injecting AWS secret')} ${chalk.magenta(`${key}`)} as ${chalk.green(prefixedKey)}`)
-      // eslint-disable-next-line no-param-reassign
-      netlifyConfig.build.environment[prefixedKey] = value
+
+      // no context, inject to all
+      if (!context) {
+        console.log(`${chalk.bold('Injecting AWS secret')} ${chalk.magenta(`${key}`)} as ${chalk.green(prefixedKey)}`)
+        // eslint-disable-next-line no-param-reassign
+        netlifyConfig.build.environment[prefixedKey] = value
+        return
+      }
+
+      // inject only to matching context
+      if (CONTEXT === context) {
+        console.log(
+          `${chalk.bold('Injecting AWS secret')} ${chalk.magenta(`${key}`)} as ${chalk.green(
+            prefixedKey,
+          )} to context ${chalk.yellow(context)}`,
+        )
+        /* eslint-disable-next-line no-param-reassign */
+        netlifyConfig.build.environment[prefixedKey] = value
+      }
     })
   },
 }
